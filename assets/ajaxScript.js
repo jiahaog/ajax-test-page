eb4b4ee72c7b6f6ca91448bcd86dee8dd83f0c9f
Jia Hao <jiahaog@gmail.com>
2015-09-19 20:23:14 +0800

    Shift script to external file

---
+++ assets/ajaxScript.js
@@ -0,0 +1,15 @@
+/**
+ * Created by JiaHao on 19/9/15.
+ */
+
+setTimeout(function () {
+
+    var paragraph = document.createElement('div');
+    paragraph.setAttribute('id', 'setTimeoutContent');
+    // BUBBLES HI is not contained in the dom initially, used to test to check if the
+    // dom contains the string
+    var text = document.createTextNode('BUBBLES HI');
+    paragraph.appendChild(text);
+
+    document.getElementById('interval-test').appendChild(paragraph);
+}, 5000);
