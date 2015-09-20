387c550491b5c932dded822adafb99f7e83578af
Jia Hao <jiahaog@gmail.com>
2015-09-20 22:31:56 +0800

    Add better descriptors and a function to show the dropdown result

--- assets/ajaxScript.js
+++ assets/ajaxScript.js
@@ -4,8 +4,11 @@
 
 var WAIT_INTERVAL = 2000;
 
-setTimeout(function () {
+/**
+ * Delayed appearance of text
+ */
 
+setTimeout(function () {
     var paragraph = document.createElement('div');
     paragraph.setAttribute('id', 'setTimeoutContent');
     // BUBBLES HI is not contained in the dom initially, used to test to check if the
@@ -15,3 +18,19 @@ setTimeout(function () {
 
     document.getElementById('interval-test').appendChild(paragraph);
 }, WAIT_INTERVAL);
+
+/**
+ * Showing the result of the dropdown box
+ */
+
+function showDropdownResult() {
+    document.querySelector('#dropdown-selected-result').innerHTML = document.querySelector('#dropdown').value;
+}
+
+// show dropdown result immediately
+showDropdownResult();
+
+// set listener
+document.querySelector('#dropdown').addEventListener('change', function(){
+    showDropdownResult();
+});
